cad8364b9ba40a6ed1e11640c11793b027ebd844
Merthan <merthan.kavak@gmail.com>
2023-06-25 13:25:09 +0300

    Added toolbar buttons

--- app/src/main/java/com/mkavaktech/reciperoamer/ui/food_details/FoodDetailsActivity.kt
+++ app/src/main/java/com/mkavaktech/reciperoamer/ui/food_details/FoodDetailsActivity.kt
@@ -1,12 +1,40 @@
 package com.mkavaktech.reciperoamer.ui.food_details
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.mkavaktech.reciperoamer.R
 
+
 class FoodDetailsActivity : AppCompatActivity() {
+
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_food_details)
+        val toolbar = findViewById<Toolbar>(R.id.toolBar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.food_details_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
