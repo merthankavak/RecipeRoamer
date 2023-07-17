e163b241e59042466ec588f6880ffe97d57d38e3
Merthan <merthan.kavak@gmail.com>
2023-07-17 14:39:31 +0300

    Bubble bottom nav bar added

--- app/src/main/java/com/mkavaktech/reciperoamer/ui/MainActivity.kt
+++ app/src/main/java/com/mkavaktech/reciperoamer/ui/MainActivity.kt
@@ -2,22 +2,40 @@ package com.mkavaktech.reciperoamer.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.Navigation
-import androidx.navigation.ui.NavigationUI
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.mkavaktech.reciperoamer.R
+import com.mkavaktech.reciperoamer.databinding.ActivityMainBinding
+import com.mkavaktech.reciperoamer.utils.Helper
 import dagger.hilt.android.AndroidEntryPoint
+import io.ak1.BubbleTabBar
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var bubbleTabBar: BubbleTabBar
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val bottomNavigationBar = findViewById<BottomNavigationView>(R.id.bottomNavBar)
-        val navController = Navigation.findNavController(this, R.id.navHostFragment)
-
-        NavigationUI.setupWithNavController(bottomNavigationBar,navController)
-
+        bubbleTabBar = binding.bottomNavBar
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+        setupBottomNavBar()
     }
+
+    private fun setupBottomNavBar() {
+        bubbleTabBar.addBubbleListener { id ->
+            Helper.onNavDestinationSelected(id,navController)
+        }
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            bubbleTabBar.setSelectedWithId(destination.id, false)
+        }
+    }
+
+
+
 }
